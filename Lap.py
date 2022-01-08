14142d9266b987a6a191e63dc32d0a0b6342ec89
Colton Wagner <cwagner315@gmail.com>
2022-01-08 15:11:12 -0600

    Updated the VotesRequired..

--- Lap.py
+++ Lap.py
@@ -16,6 +16,9 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 PIPEDOWN = os.getenv('DISCORD_PIPE_DOWN_ID')
 
+intents = discord.Intents.default()
+intents.members = True
+
 targetMember = None
 acussingMember = None
 voteIds = []
@@ -23,7 +26,7 @@ voteActive = False
 voteChannel = None
 voteUserId = ''
 voteStartTime = datetime.now()
-votesRequired = 5
+votesRequired = 0
 
 muteActive = False
 muteTime = 0
@@ -54,20 +57,20 @@ async def countdownLoop():
         elif timeDelta == 62:
             voteActive = False
             voteIds = []
-            await channel.send(f'Votes now closed. Better luck next time fuck nugget {acussingMember.mention}!')
+            await channel.send(f'Votes now closed. Better luck next time fuck nugget @{acussingMember.name}!')
 
     muteTimeDelta = (datetime.now() - muteStartTime)
     muteTimeDelta = math.floor(muteTimeDelta.total_seconds())
 
     if muteActive:
         if muteTimeDelta == muteTime:
-            await channel.send(f'The {muteTime} second mute has been lifted! {targetMember.mention}')
+            await channel.send(f'The {muteTime} second mute has been lifted! @{targetMember.name}')
             muteActive = False
             muteTime = 0
             await set_to_unmuted(targetMember)
 
 
-@bot.command(help = 'Intiates a one minute vote for (3/5) of the target members current voice channel non-self-defeaned members to cast a vote by typing !lap\rif the mute is successful it will server mute the target user for a random amount of seconds between 15-120.\nCommand use: !lap Optional[target] to start a vote.\r\rex: \r\t!lap @pipe_down\r\t!lap # Defaults: @pipe_down\r')
+@bot.command(intents = intents, help = 'Intiates a one minute vote for (3/5) of the target members current voice channel non-self-defeaned members to cast a vote by typing !lap\rif the mute is successful it will server mute the target user for a random amount of seconds between 15-120.\nCommand use: !lap Optional[target] to start a vote.\r\rex: \r\t!lap @pipe_down\r\t!lap # Defaults: @pipe_down\r')
 async def lap(ctx, target : Optional[discord.Member] = None):
     global voteIds, voteActive, voteChannel, voteStartTime, votesRequired, muteStartTime, muteTime, muteActive, targetMember, acussingMember
     
@@ -83,22 +86,31 @@ async def lap(ctx, target : Optional[discord.Member] = None):
             target = targetMember
 
         if ctx.author.id in voteIds:
-            response = f'You can\'t vote twice fuck face! Yes.. I am talking to you {ctx.author.mention}'
+            response = f'You can\'t vote twice fuck face! Yes.. I am talking to you @{ctx.author.name}'
         else:
             voteIds.insert(len(voteIds), ctx.author.id)
 
             if len(voteIds) == 1:
-                voiceMembers = target.voice.channel.members
-                if voiceMembers:
+                if target.voice:
+                    channel = target.voice.channel
+                    voiceMembers = channel.members
+                    print(f"Total Members: {len(voiceMembers)}")
                     votesRequired = 0
                     for member in voiceMembers:
+                        print(f"Member: {member.name}")
                         if not member.voice.self_deaf:
                             votesRequired = votesRequired + 1
-                            print(f"Votes Required: {votesRequired}")
+                            print(f"Added Vote: {votesRequired}")
+                    
+                    if votesRequired > 0:
+                        votesRequired = math.floor(votesRequired * (3/5))
 
-                votesRequired = math.floor(votesRequired * (3 / 5))
-                votesRequired = votesRequired and (votesRequired >= 2) or 2 
-                response = f'{ctx.author.mention} has intiated a vote to mute {target.mention}! "!lap" to vote ({len(voteIds)} / {votesRequired})'
+                print(f"Pre-Votes Required: {votesRequired}")
+                if not (votesRequired >= 3):
+                    votesRequired = 3
+                
+                print(f"Final Votes Required: {votesRequired}")
+                response = f'@{ctx.author.name} has intiated a vote to mute @{target.name}! "!lap" to vote ({len(voteIds)} / {votesRequired})'
                 voteActive = True
                 voteStartTime = datetime.now()
                 voteChannel = ctx.channel
@@ -106,15 +118,15 @@ async def lap(ctx, target : Optional[discord.Member] = None):
                 muteTime = random.randint(15, 120)
                 muteStartTime = datetime.now()
                 muteActive = True
-                response = f'{ctx.author.mention} put the final nail in the coffin! ({len(voteIds)} / {votesRequired})\n{target.mention} will be muted for {muteTime} seconds!'
+                response = f'@{ctx.author.name} put the final nail in the coffin! ({len(voteIds)} / {votesRequired})\n@{target.name} will be muted for {muteTime} seconds!'
                 voteIds = []
                 voteActive = False
                 await set_to_muted(target)
             else:
-                response = f'{ctx.author.mention} has spoken! ({len(voteIds)} / 5)'
+                response = f'@{ctx.author.name} has spoken! ({len(voteIds)} / {votesRequired})'
         await ctx.send(response)
     else:
-        await ctx.send(f"I am currently muting {targetMember.mention}.. like tf chill out and get the fuck outta my face {ctx.author.mention} dumb ass bitch!")
+        await ctx.send(f"I am currently muting @{targetMember.name}.. like tf chill out and get the fuck outta my face @{ctx.author.name} dumb ass bitch!")
 
 async def set_to_muted(member: discord.Member):
     await member.edit(mute=True)
